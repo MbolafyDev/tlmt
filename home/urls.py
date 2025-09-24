d0ce2f41a739960e0819bc3f32d60edbd51190f8
Mbolafy.Dev <mbolafy.dev@gmail.com>
2025-09-24 12:29:05 +0300

    ajout du template, voir detail sur le template home

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="/home")
+    path('', views.home, name="/home"),
+    path('produit/<int:produit_id>/', views.produit_detail, name='produit_detail'),
 ]
