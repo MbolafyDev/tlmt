b5b55ee6f3a338a148d9cdd64c0ffbb5f55e15f7
Mbolafy.Dev <mbolafy.dev@gmail.com>
2025-09-24 14:20:36 +0300

    rend dynamique le voir detail de la pages

--- home/views.py
+++ home/views.py
@@ -24,29 +24,10 @@ def home(request):
 
 
 def produit_detail(request, produit_id):
-    # Ici on suppose que vous avez un modèle Produit
-    # produit = get_object_or_404(Produit, id=produit_id)
-
-    # Exemple statique pour démonstration
-    produit = {
-        'id': produit_id,
-        'nom': 'Smartwatch T800 Ultra',
-        'description_courte': 'Montre connectée sportive avec écran HD, compatible Android & iOS.',
-        'avis': 45,
-        'prix': 299000,
-        'prix_original': 350000,
-        'caracteristiques': [
-            'Écran 2.0” HD',
-            'Bluetooth 5.0',
-            'Mesure fréquence cardiaque',
-            'Autonomie : 5 jours',
-            'Étanche IP68'
-        ],
-        'couleurs': ['Orange', 'Noir', 'Argent'],
-        'marque': 'Zara Tech',
-        'image': 'images/20250603_Smartwatch T800 Ultra_Zara Store.png',
-    }
-
+    produit = get_object_or_404(
+        Produit.objects.prefetch_related("images", "caracteristiques", "couleurs"),
+        id=produit_id
+    )
     return render(request, 'home/detail.html', {'produit': produit})
 
 def checkout(request):
